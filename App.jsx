eff5d6ef9969b48025636474b131e5624e693fa7
WASIMAKRAM18 <85391802+WASIMAKRAM18@users.noreply.github.com>
2021-06-28 16:18:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Flooter from "./Flooter"
+import Info from "./Info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Flooter />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+export default App;
